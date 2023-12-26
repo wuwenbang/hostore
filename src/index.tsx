7ff068b6491e25019466777f94d6d4780d959162
wuwenbang <wuwenbang@foxmail.com>
2023-12-26 17:08:31 +0800

    fix: ts strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -15,9 +15,8 @@ import {
 const useSafeEffect = typeof window === "undefined" ? useEffect : useLayoutEffect;
 
 export function createStore<Value, Props = void>(useHook: (initialProps?: Props) => Value) {
-  const Context = createContext<MutableRefObject<Value>>(null);
-  const EventContext = createContext<Set<(value: Value) => void>>(null);
-
+  const Context = createContext<MutableRefObject<Value>>(undefined as unknown as MutableRefObject<Value>);
+  const EventContext = createContext<Set<(value: Value) => void>>(undefined as unknown as Set<(value: Value) => void>);
   const Provider: FC<PropsWithChildren<{ initialProps?: Props }>> = memo(({ initialProps, children }) => {
     const value = useHook(initialProps);
     const ref = useRef(value);
@@ -68,7 +67,7 @@ export function createStore<Value, Props = void>(useHook: (initialProps?: Props)
 }
 
 export function useMethod<Args extends unknown[], Return>(fn: (...args: Args) => Return): (...args: Args) => Return {
-  const ref = useRef<typeof fn | undefined>(undefined);
+  const ref = useRef(fn);
 
   useSafeEffect(() => {
     ref.current = fn;
