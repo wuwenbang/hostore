d9eec0578da8a1190fe84bc21f1776a9ac71ce51
wuwenbang <wuwenbang@foxmail.com>
2023-12-26 20:02:18 +0800

    build: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,67 @@
-import path from "path";
 import resolve from "@rollup/plugin-node-resolve";
 import cjs from "@rollup/plugin-commonjs";
-import ts from "rollup-plugin-typescript2";
+import ts from "@rollup/plugin-typescript";
+import generatePackageJson from "rollup-plugin-generate-package-json";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
 
-const inputPath = path.resolve(__dirname, "./index.ts");
-const outputPath = path.resolve(__dirname, "./dist/index.js");
-
-export default {
-  input: inputPath,
+const unpkgRollupConfig = {
+  input: "./index.ts",
   output: {
-    file: outputPath,
-    format: "umd",
     name: "hook-store",
+    file: "dist/index.min.js",
+    format: "umd",
   },
-  external: ["react"],
-  plugins: [resolve(), ts(), cjs()],
+  external: ["react", "react/jsx-runtime"],
+  plugins: [
+    resolve(),
+    babel({
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+      exclude: "node_modules/**",
+      babelHelpers: "bundled",
+      presets: ["@babel/env", "@babel/preset-react"],
+    }),
+    terser(),
+    cjs(),
+    ts(),
+    generatePackageJson({
+      inputFolder: "./",
+      outputFolder: "./dist/",
+      baseContents: (pkg) => pkg,
+    }),
+  ],
 };
+
+const mainRollupConfig = {
+  input: "./index.ts",
+  output: [
+    {
+      file: "dist/index.cjs.js",
+      format: "umd",
+      name: "hook-store",
+    },
+    {
+      file: "dist/index.es.js",
+      format: "es",
+    },
+  ],
+  external: ["react", "react/jsx-runtime"],
+  plugins: [
+    resolve(),
+    babel({
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+      exclude: "node_modules/**",
+      babelHelpers: "bundled",
+      presets: ["@babel/env", "@babel/preset-react"],
+    }),
+    cjs(),
+    ts(),
+    generatePackageJson({
+      inputFolder: "./",
+      outputFolder: "./dist/",
+      baseContents: (pkg) => pkg,
+    }),
+  ],
+};
+
+export default [mainRollupConfig, unpkgRollupConfig];
