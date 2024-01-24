259f37b84edebf8198368df1a4d7442d1a040de7
wuwenbang <wuwenbang@foxmail.com>
2024-01-24 12:06:23 +0800

    chore: add test enviroment

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import cjs from "@rollup/plugin-commonjs";
 import ts from "@rollup/plugin-typescript";
 import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
+const babelConfig = require("./babel.config");
 
 const unpkgRollupConfig = {
   input: "src/index.tsx",
@@ -18,7 +19,7 @@ const unpkgRollupConfig = {
       extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
       exclude: "node_modules/**",
       babelHelpers: "bundled",
-      presets: ["@babel/env", "@babel/preset-react"],
+      ...babelConfig,
     }),
     terser(),
     cjs(),
@@ -30,9 +31,9 @@ const mainRollupConfig = {
   input: "src/index.tsx",
   output: [
     {
+      name: "hostore",
       file: "dist/index.cjs.js",
       format: "umd",
-      name: "hostore",
     },
     {
       file: "dist/index.es.js",
@@ -46,7 +47,7 @@ const mainRollupConfig = {
       extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
       exclude: "node_modules/**",
       babelHelpers: "bundled",
-      presets: ["@babel/env", "@babel/preset-react"],
+      ...babelConfig,
     }),
     cjs(),
     ts(),
