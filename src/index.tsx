9ebad945da9c18fe3dda0dfd48b9e9843bdd81f9
wuwenbang <wuwenbang@foxmail.com>
2024-01-24 12:02:09 +0800

    fix: cannot update a component while rendering a different component warning

--- src/index.tsx
+++ src/index.tsx
@@ -22,8 +22,10 @@ export function createStore<Value, Props>(useHook: (props: Props) => Value) {
     const ref = useRef(value);
     const events = useRef<Set<(value: Value) => void>>(new Set()).current;
     ref.current = value;
-    events.forEach((event) => {
-      event(value);
+    useSafeEffect(() => {
+      events.forEach((event) => {
+        event(value);
+      });
     });
     return (
       <Context.Provider value={ref}>
@@ -51,9 +53,7 @@ export function createStore<Value, Props>(useHook: (props: Props) => Value) {
         if (current.value === nextValue) return;
         const nextSelectedValue = current.selector ? current.selector(nextValue) : nextValue;
         if (current.selectedValue === nextSelectedValue) return;
-        setTimeout(() => {
-          forceUpdate();
-        });
+        forceUpdate();
       };
       ref.current.events.add(event);
       return () => {
@@ -62,16 +62,13 @@ export function createStore<Value, Props>(useHook: (props: Props) => Value) {
     }, []);
     return selectedValue as SelectedValue;
   }
-
   return { Provider, useStore };
 }
 
 export function useMethod<Args extends unknown[], Return>(fn: (...args: Args) => Return): (...args: Args) => Return {
   const ref = useRef(fn);
-
   useSafeEffect(() => {
     ref.current = fn;
   });
-
   return useCallback((...args: Args) => ref.current(...args), []);
 }
