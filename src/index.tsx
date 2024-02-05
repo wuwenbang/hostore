11931e7ca172ffff74b3de21174d0dd900d9544f
wuwenbang <wuwenbang@foxmail.com>
2024-02-05 15:10:56 +0800

    fix: remove memo of Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import {
-  createContext,
-  FC,
-  memo,
   PropsWithChildren,
+  createContext,
   useCallback,
   useContext,
   useEffect,
@@ -12,16 +10,17 @@ import {
 } from "react";
 
 const useSafeEffect = typeof window === "undefined" ? useEffect : useLayoutEffect;
+
 interface ContextObject<Value> {
   value: Value;
   events: Set<(value: Value) => void>;
 }
+
 export function createStore<Value, Props>(useHook: (props: Props) => Value) {
   const Context = createContext<ContextObject<Value>>({} as unknown as ContextObject<Value>);
-  const Provider: FC<PropsWithChildren<{ props?: Props }>> = memo(({ children, props }) => {
+  function Provider({ children, props }: PropsWithChildren<{ props?: Props }>) {
     const value = useHook(props as Props);
-    const events: Set<(value: Value) => void> = new Set();
-    const context = useRef({ value, events }).current;
+    const context = useRef<ContextObject<Value>>({ value, events: new Set() }).current;
     context.value = value;
     useSafeEffect(() => {
       context.events.forEach((event) => {
@@ -29,7 +28,7 @@ export function createStore<Value, Props>(useHook: (props: Props) => Value) {
       });
     });
     return <Context.Provider value={context}>{children}</Context.Provider>;
-  });
+  }
   function useStore<SelectedValue = Value>(selector?: (value: Value) => SelectedValue): SelectedValue {
     const [, forceUpdate] = useReducer(() => ({}), {});
     const { value, events } = useContext(Context);
@@ -43,13 +42,13 @@ export function createStore<Value, Props>(useHook: (props: Props) => Value) {
     const ref = useRef(storeValue);
     ref.current = storeValue;
     useSafeEffect(() => {
-      const event = (nextValue: Value) => {
+      function event(nextValue: Value) {
         const { value, selectedValue, selector } = ref.current;
         if (value === nextValue) return;
         const nextSelectedValue = selector ? selector(nextValue) : nextValue;
         if (selectedValue === nextSelectedValue) return;
         forceUpdate();
-      };
+      }
       ref.current.events.add(event);
       return () => {
         ref.current.events.delete(event);
